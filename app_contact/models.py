f36657f1d4c1d7069f9078f6142ebd56334d5d33
nogueirarcz <nogueira@developer.com>
2024-09-22 21:29:32 -0300

    Criando e registrando model Contact

--- app_contact/models.py
+++ app_contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+
+        return f'{self.first_name} {self.last_name}'
+    
