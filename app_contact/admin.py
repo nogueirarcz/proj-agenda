451b87544d990d68181a8d399feea074b4bc5398
nogueirarcz <nogueira@developer.com>
2024-09-26 21:33:57 -0300

    Customizando o contacts.admim

--- app_contact/admin.py
+++ app_contact/admin.py
@@ -5,5 +5,18 @@ from app_contact.models import Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
 
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
     
+    ordering = ('-id',)
+
+    list_filter = ('created_date',)
+
+    search_fields = ('first_name', 'second_name',)
+
+    list_per_page = 10
+
+    list_max_show_all = 100
+
+    list_editable = ('first_name', 'last_name',)
+
+    list_display_links = ('id')
